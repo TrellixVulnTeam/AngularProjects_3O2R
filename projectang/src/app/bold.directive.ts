07f4b88e936be749e7f7b2fe9dd91da9fc77b259
Oleksii Borovyk <oleksiib@sweetsoft.com.ua>
2021-06-09 18:58:14 +0300

    Add directive usage in projectang

--- projectang/src/app/bold.directive.ts
+++ projectang/src/app/bold.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 @Directive({
 
     selector:'[bold]'
@@ -7,8 +7,28 @@ export class BoldDirective{
     /**
      *
      */
-    constructor(private elemRef:ElementRef) {
-       this.elemRef.nativeElement.style.fontWeight="bold";
+    @Input("bold") selectedSize="18px";
+    @Input() defaultSize="16px";
+    private fontSize:string;
+    constructor(private elemRef:ElementRef, private renderer:Renderer2) {
+
+        this.fontSize=this.defaultSize;
         
+       //this.elemRef.nativeElement.style.fontWeight="bold";
+        this.renderer.setStyle(this.elemRef.nativeElement, "cursor", "pointer");
+        
+    }
+    @HostListener("mouseenter") onMouseEnter(){
+        this.setFontWeight("bold");
+    }
+
+
+    @HostListener("mouseleave") onMouseLeave()
+    {
+        this.setFontWeight("normal");
+    }
+    setFontWeight(width:string)
+    {
+        this.renderer.setStyle(this.elemRef.nativeElement, "font-weight", width);
     }
 }
